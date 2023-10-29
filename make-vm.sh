c765548240bad2ba6cce94d6268ce5a6b1c9987e
John Finigan <johnwfinigan@gmail.com>
2023-10-29 18:35:26 -0500

    add centos7 support

--- make-vm.sh
+++ make-vm.sh
@@ -5,7 +5,8 @@ set -eu
 export PATH=/usr/bin
 
 memory_mb=4096
-while getopts :m: opt; do
+vers=9
+while getopts :m:v: opt; do
   case "$opt" in
   m)
     re='^[0-9]+$'
@@ -15,6 +16,9 @@ while getopts :m: opt; do
       echo "Error - memory must be specified as a number" >&2
       exit 112
     fi
+    ;;
+  v)
+    vers="$OPTARG"
     ;;
   \?)
     echo "invalid option, exiting" >&2
@@ -37,11 +41,19 @@ vm_name="$1"
 
 loc=/var/lib/libvirt/images
 destdisk="$loc/$vm_name.qcow2"
-srcdisk="$loc/Rocky-8-GenericCloud.latest.x86_64.qcow2"
+
+url_prefix="https://download.rockylinux.org/pub/rocky/${vers}/images/x86_64/"
+url_file="Rocky-${vers}-GenericCloud.latest.x86_64.qcow2"
+if [ $vers = 7 ]; then
+  url_prefix="https://cloud.centos.org/centos/7/images/"
+  url_file="CentOS-7-x86_64-GenericCloud.qcow2"
+fi
+
+srcdisk="$loc/$url_file"
 
 if ! sudo stat "$srcdisk"; then
   img=$(mktemp)
-  curl https://download.rockylinux.org/pub/rocky/8/images/Rocky-8-GenericCloud.latest.x86_64.qcow2 >"$img"
+  curl "${url_prefix}${url_file}" >"$img"
   sudo cp --sparse=always "$img" "$srcdisk"
   rm "$img"
   if [ -f /usr/sbin/restorecon ]; then
@@ -51,21 +63,25 @@ else
   echo template image already exists at "$srcdisk"
 fi
 
-sudo cp -a --sparse=always "$srcdisk" "$destdisk"
+sudo cp -a --sparse=always --reflink=auto "$srcdisk" "$destdisk"
 sudo virt-sysprep --operations=defaults -a "$destdisk"
 
 meta=$(mktemp)
 printf "instance-id: %s\n" "$(uuidgen)" >"$meta"
 
+cloudinit=$(mktemp)
+sed -e "s/XXX_HOSTNAME/$vm_name/" < cloud-init-el.yml > "$cloudinit"
+
+
 sudo virt-install \
   --name "$1" \
-  --os-variant rhel8.4 \
+  --os-variant "rhel${vers}.0" \
   --memory "$memory_mb" \
   --vcpus 2 \
   --network default \
   --import \
   --disk "$destdisk" \
   --noautoconsole \
-  --cloud-init disable=on,user-data=cloud-init-rocky.yml,meta-data="$meta"
+  --cloud-init disable=on,user-data="$cloudinit",meta-data="$meta"
 
-rm "$meta"
+rm "$meta" "$cloudinit"
